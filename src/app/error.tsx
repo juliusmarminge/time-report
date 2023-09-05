133e6f74143cc4835402951ed92dec515b81927c
juliusmarminge <julius0216@outlook.com>
2023-09-05 19:54:04 +0200

    error.js

---
+++ src/app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { DashboardShell } from "~/components/dashboard-shell";
+import { Button } from "~/ui/button";
+
+export default function Error(props: { error: Error; reset: () => void }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(props.error);
+  }, [props.error]);
+
+  return (
+    <DashboardShell title="Error">
+      <div className="flex h-[600px] flex-col items-center justify-center gap-4 rounded-lg border border-dashed">
+        <h2 className="text-xl font-bold">{`Something went wrong`}</h2>
+        <Button onClick={props.reset}>Try again</Button>
+      </div>
+    </DashboardShell>
+  );
+}
