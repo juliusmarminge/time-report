63fb740709a111e049b03fa7740c2688759590e1
Julius Marminge <julius0216@outlook.com>
2023-09-05 14:13:44 +0200

    fix relational query support (#3)
    * eh??
    
    * don't need relation name

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import type { AdapterAccount } from "@auth/core/adapters";
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   bigint,
   decimal,
@@ -72,11 +72,9 @@ export const users = table("user", {
   image: varchar("image", { length: 255 }),
 });
 
-// I don't need relational queries for now, and I don't quite understand how to
-// make drizzle studio work with them, so I'm leaving this commented out for now.
-// export const usersRelations = relations(users, ({ many }) => ({
-//   accounts: many(accounts),
-// }));
+export const usersRelations = relations(users, ({ many }) => ({
+  accounts: many(accounts),
+}));
 
 export const accounts = table(
   "account",
@@ -101,9 +99,9 @@ export const accounts = table(
   }),
 );
 
-// export const accountsRelations = relations(accounts, ({ one }) => ({
-//   user: one(users, { fields: [accounts.userId], references: [users.id] }),
-// }));
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+}));
 
 export const sessions = table(
   "session",
@@ -119,9 +117,9 @@ export const sessions = table(
   }),
 );
 
-// export const sessionsRelations = relations(sessions, ({ one }) => ({
-//   user: one(users),
-// }));
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
 
 export const verificationTokens = table(
   "verificationToken",
