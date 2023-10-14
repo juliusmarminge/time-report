2d9a4dc748b3027c19fa9eb12977a2fedf8c36d1
juliusmarminge <julius0216@outlook.com>
2023-10-14 22:24:24 +0200

    tidy up signup page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
-import { DashboardShell } from "~/components/dashboard-shell";
 import { CSRF_experimental } from "~/lib/auth";
 import { Button } from "~/ui/button";
 
@@ -8,14 +7,24 @@ export const runtime = "edge";
 
 export default function LoginPage() {
   return (
-    <DashboardShell title="Sign in">
-      <form action={`/api/auth/signin/github`} method="post">
-        <Button type="submit" variant="accent">
-          <GitHubLogoIcon className="mr-2 h-4 w-4" />
-          Sign in with GitHub
-        </Button>
-        <CSRF_experimental />
-      </form>
-    </DashboardShell>
+    <div className="relative flex h-full min-h-screen w-full flex-col items-center justify-center">
+      <div className="z-50 flex w-full max-w-md flex-col gap-4 rounded-3xl bg-muted/60 p-12 shadow-lg">
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-xl font-extrabold">Sign in</h1>
+            <p>
+              to continue to <b>Time Report</b>
+            </p>
+          </div>
+        </div>
+        <form action="/api/auth/signin/github" method="post">
+          <Button type="submit" className="px-10">
+            <GitHubLogoIcon className="mr-2 h-4 w-4" />
+            Sign in with GitHub
+          </Button>
+          <CSRF_experimental />
+        </form>
+      </div>
+    </div>
   );
 }
