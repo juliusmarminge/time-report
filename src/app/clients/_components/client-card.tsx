c356df2440229acc2eacd6c25c3949eea5d1876f
juliusmarminge <julius0216@outlook.com>
2023-10-15 13:45:31 +0200

    billing periods

--- src/app/clients/_components/client-card.tsx
+++ src/app/clients/_components/client-card.tsx
@@ -114,7 +114,7 @@ export function ClientCard(props: { client: Client }) {
         </h3>
 
         <div className="flex flex-col gap-2">
-          <h3 className="text-base font-semibold">Periods</h3>
+          <h3 className="text-base font-semibold">Billing Periods</h3>
           <ul className="flex flex-col gap-2">
             {sortedPeriods.map((p, i) => (
               <div className="flex flex-col gap-1">
