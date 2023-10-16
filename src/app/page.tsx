d7fae358d88eeaa161cb2ded00f493d573e0237e
juliusmarminge <julius0216@outlook.com>
2023-10-16 23:05:28 +0200

    simple info landing page

---
+++ src/app/page.tsx
@@ -0,0 +1,74 @@
+import { DashboardShell } from "~/components/dashboard-shell";
+
+export default function Page() {
+  return (
+    <DashboardShell
+      title="Time Report"
+      description="Welcome to Time Report, a simple time reporting app for indie
+      contractors."
+      className="max-w-2xl gap-6"
+    >
+      <div className="flex flex-col gap-3">
+        <h2 className="text-lg font-semibold">How to use?</h2>
+        <p className="text-base">
+          Time Report has 3 main concepts, <b>Clients</b>, <b>Periods</b> and{" "}
+          <b>Slots</b>.
+        </p>
+        <h3 className="font-semibold">Clients</h3>
+        <p className="text-base">
+          Clients are... well your clients that you report time for. When
+          creating clients you'll provide the currency that you invoice them in,
+          your default hourly rate as well as what billing period you use for
+          that client. For example, you may invoice a client $100 USD per hour
+          and send them an invoice every month.
+        </p>
+
+        <h3 className="font-semibold">Periods</h3>
+        <p className="text-base">
+          Periods corresponds to the billing period that you use for a client.
+          For the above example, you'll have a period for each month. At the end
+          of the month, you'll get notified to close that period and create a
+          new one so that you can easily keep track of what you have invoiced
+          and not.
+        </p>
+
+        <h3 className="font-semibold">Slots</h3>
+        <p className="text-base">
+          Each period gets filled with a bunch of Slots. Slots are time entries
+          with a duration, currency and amount (which defaults to your default
+          hourly rate for that client). You can add as many slots as you want to
+          a period and Time Report will calculate the total amount for you.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h2 className="text-lg font-semibold">Core Features</h2>
+        <p>
+          Time Report aims to be as simple as possible, the main features we aim
+          to solve are:
+        </p>
+
+        <h3>Easily report time for multiple clients</h3>
+        <p>
+          Most existing apps are made for companies with lots of employees that
+          reports their time for a single company. Time Report takes a different
+          stance and focuses on contractors that invoice multiple companies.
+        </p>
+
+        <h3 className="font-semibold">Tracking your month</h3>
+        <p>
+          Easily keep track of how many hours you've worked for each of your
+          clients, and how much you'll end up invoicing them at the end of the
+          period. You'll also get aggregate information for all your differnet
+          clients in a single place.
+        </p>
+
+        <h3 className="font-semibold">Automatic currency converting.</h3>
+        <p>
+          Invoicing clients in different currencies? No worries, set your
+          preferred currency and we'll convert the total to that currency.
+        </p>
+      </div>
+    </DashboardShell>
+  );
+}
