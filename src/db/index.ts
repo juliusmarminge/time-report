0bb93abbfd75158a739271c1fa42139d01866831
juliusmarminge <julius0216@outlook.com>
2023-10-25 07:51:29 -0700

    cmt out completely if not used

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 import { Client } from "@planetscale/database";
 import type { Logger } from "drizzle-orm";
-import { drizzle as defaultDrizzle } from "drizzle-orm/mysql2";
-import type { PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless";
+// import { drizzle as defaultDrizzle } from "drizzle-orm/mysql2";
+// import type { PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless";
+// import { createPool } from "mysql2/promise";
 import { drizzle as pscaleDrizzle } from "drizzle-orm/planetscale-serverless";
-import { createPool } from "mysql2/promise";
 
 import * as schema from "./schema";
 
@@ -12,34 +12,27 @@ class _MyLogger implements Logger {
     console.log("[DRIZZLE]", { query, params });
   }
 }
-function createDrizzle() {
-  if (process.env.USE_OFFLINE) {
-    const db = defaultDrizzle(
-      createPool({
-        host: "localhost",
-        user: "root",
-        port: 3306,
-        database: "time-report",
-      }),
-      {
-        schema,
-        mode: "default",
-        // logger: new _MyLogger(),
-      },
-    );
 
-    return db;
-  }
+export const db = pscaleDrizzle(
+  new Client({
+    url: process.env.DATABASE_URL,
+  }).connection(),
+  {
+    schema,
+    // logger: new _MyLogger(),
+  },
+);
 
-  return pscaleDrizzle(
-    new Client({
-      url: process.env.DATABASE_URL,
-    }).connection(),
-    {
-      schema,
-      // logger: new _MyLogger(),
-    },
-  );
-}
-
-export const db = createDrizzle() as PlanetScaleDatabase<typeof schema>;
+// export const db = defaultDrizzle(
+//   createPool({
+//     host: "localhost",
+//     user: "root",
+//     port: 3306,
+//     database: "time-report",
+//   }),
+//   {
+//     schema,
+//     mode: "default",
+//     // logger: new _MyLogger(),
+//   },
+// ) as unknown as PlanetScaleDatabase<typeof schema>;
