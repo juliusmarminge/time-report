84645821a0f26b417e80b6dcc95e656d7a769d2f
juliusmarminge <julius0216@outlook.com>
2023-10-25 07:37:31 -0700

    uath beta

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import { Client } from "@planetscale/database";
 import type { Logger } from "drizzle-orm";
 import { drizzle as defaultDrizzle } from "drizzle-orm/mysql2";
-import type { PlanetScaleDatabase} from "drizzle-orm/planetscale-serverless";
+import type { PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless";
 import { drizzle as pscaleDrizzle } from "drizzle-orm/planetscale-serverless";
 import { createPool } from "mysql2/promise";
 
@@ -16,7 +16,10 @@ function createDrizzle() {
   if (process.env.USE_OFFLINE) {
     const db = defaultDrizzle(
       createPool({
-        uri: process.env.DATABASE_URL,
+        host: "localhost",
+        user: "root",
+        port: 3306,
+        database: "time-report",
       }),
       {
         schema,
@@ -25,7 +28,7 @@ function createDrizzle() {
       },
     );
 
-    return db
+    return db;
   }
 
   return pscaleDrizzle(
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,29 @@
+// import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 import { auth } from "~/lib/auth";
 
 const publicRoutes = ["/", "/login(.*)", "/api(.*)"];
 
+const isPublic = (url: URL) =>
+  publicRoutes.some((route) => new RegExp(`^${route}$`).test(url.pathname));
+
 export default auth((req) => {
   const url = req.nextUrl;
   const auth = req.auth;
 
-  if (url.pathname === "/login" && auth.user) {
+  if (url.pathname === "/login" && auth?.user) {
     // User is already signed in, redirect to app
     url.pathname = "/";
     return NextResponse.redirect(url);
   }
 
-  if (
-    publicRoutes.some((route) => new RegExp(`^${route}$`).test(url.pathname))
-  ) {
+  if (isPublic(url)) {
     // Public path, go ahead
     return NextResponse.next();
   }
 
-  if (!auth.user) {
+  if (!auth?.user) {
     // Protected path and user is not signed in, redirect to signin
     url.pathname = "/login";
     return NextResponse.redirect(url);
@@ -29,6 +31,17 @@ export default auth((req) => {
 
   return NextResponse.next();
 });
+
+// Dev
+// export default function middleware(request: NextRequest) {
+//   const url = request.nextUrl;
+//   if (isPublic(url)) {
+//     // Public path, go ahead
+//     return NextResponse.next();
+//   }
+
+//   return NextResponse.next();
+// }
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
