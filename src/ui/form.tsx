1132d5f45a5a1ac702d4098be7c351ae37a9c4cb
juliusmarminge <julius0216@outlook.com>
2023-12-04 00:46:13 +0100

    ok

--- src/ui/form.tsx
+++ src/ui/form.tsx
@@ -26,7 +26,6 @@ export function useForm<TSchema extends BaseSchema>(
 ) {
   const form = useFormHook<Input<TSchema>>({
     ...props,
-    // @ts-expect-error - FIXME: I guess some version broke stuff???
     resolver: valibotResolver(props.schema),
   });
 
