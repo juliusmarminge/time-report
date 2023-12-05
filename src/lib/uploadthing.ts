61c5f4ee5c6051c3a101b8ac026c0261dfeba9a9
juliusmarminge <julius0216@outlook.com>
2023-12-05 17:03:01 +0100

    commity

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -5,5 +5,5 @@ import type { UploadRouter } from "~/app/api/uploadthing/router";
 
 export const { useUploadThing } = generateReactHelpers<UploadRouter>();
 
-export const { UploadButton, UploadDropzone } =
+export const { UploadButton, UploadDropzone, Uploader } =
   generateComponents<UploadRouter>();
