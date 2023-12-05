09ba7c1742f711103799f445cfdf08b4c6ffbe9a
juliusmarminge <julius0216@outlook.com>
2023-12-05 17:02:04 +0100

    do some change

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,9 @@
+import { generateComponents } from "@uploadthing/react";
 import { generateReactHelpers } from "@uploadthing/react/hooks";
 
 import type { UploadRouter } from "~/app/api/uploadthing/router";
 
 export const { useUploadThing } = generateReactHelpers<UploadRouter>();
+
+export const { UploadButton, UploadDropzone } =
+  generateComponents<UploadRouter>();
