d44493dd11f8e59fb0e97133c8b6e32516831008
juliusmarminge <julius0216@outlook.com>
2024-01-17 14:30:11 +0100

    that didn't work either

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,12 +36,6 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
-          />
-        </head>
         <body
           className={cn(
             "min-h-screen font-sans antialiased",
