5c0f5b8ece0bb2135bd0ce36986f79c16c3062e0
juliusmarminge <julius0216@outlook.com>
2024-02-06 09:24:11 +0100

    bumps

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
   openAnalyzer: false,
 })({
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     ppr: true,
     instrumentationHook: true,
@@ -22,5 +25,10 @@ export default withBundleAnalyzer({
       },
     ],
   },
-  eslint: { ignoreDuringBuilds: true },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.ignoreWarnings = [{ module: /@opentelemetry(.*)/ }];
+    }
+    return config;
+  },
 });
