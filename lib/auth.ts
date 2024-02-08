538e489701839b9d8418a686a179bf1a3a70f461
juliusmarminge <julius0216@outlook.com>
2024-02-08 09:00:03 +0100

    import type i guess...

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { eq } from "drizzle-orm";
 import type { InferSelectModel } from "drizzle-orm";
 import NextAuth from "next-auth";
-import "next-auth/adapters";
+import type from "next-auth/adapters";
 import type { EmailConfig } from "next-auth/providers";
 import Github from "next-auth/providers/github";
 import { redirect } from "next/navigation";
