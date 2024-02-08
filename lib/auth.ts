0140cd62036ddd45858d11511d98883c195e56ed
juliusmarminge <julius0216@outlook.com>
2024-02-08 08:53:20 +0100

    chore: bump auth back up

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
-import type { Adapter } from "@auth/core/adapters";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { eq } from "drizzle-orm";
 import type { InferSelectModel } from "drizzle-orm";
 import NextAuth from "next-auth";
+import "next-auth/adapters";
 import type { EmailConfig } from "next-auth/providers";
 import Github from "next-auth/providers/github";
 import { redirect } from "next/navigation";
@@ -16,13 +16,13 @@ export type { Session } from "next-auth";
 export const providers = [{ name: "github", handler: Github }] as const;
 export type OAuthProviders = (typeof providers)[number]["name"];
 
-declare module "@auth/core/types" {
+declare module "next-auth" {
   interface Session {
     user: InferSelectModel<typeof users>;
   }
 }
 
-declare module "@auth/core/adapters" {
+declare module "next-auth/adapters" {
   interface AdapterUser extends InferSelectModel<typeof users> {}
 }
 
@@ -61,7 +61,7 @@ export const {
 
       return sessionAndUsers[0] ?? null;
     },
-  } as Adapter,
+  },
   providers: [
     ...providers.map((p) => p.handler),
     ...(process.env.VERCEL_ENV !== "production" ? [mockEmail] : []),
