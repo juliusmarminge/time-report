85463d42b23b81280c6e3f7a98878c06baab0135
juliusmarminge <julius0216@outlook.com>
2024-06-05 08:45:52 +0200

    scaffold

--- src/app/(app)/periods/[periodId]/page.tsx
+++ src/app/(app)/periods/[periodId]/page.tsx
@@ -1,11 +1,99 @@
+import { Suspense } from "react";
 import { DashboardShell } from "../../_components/shell";
+import * as trpc from "~/trpc/datalayer";
+import { notFound } from "next/navigation";
+import { formatOrdinal, isPast } from "~/lib/temporal";
+import { Badge } from "~/ui/badge";
+import { dinero, toDecimal } from "dinero.js";
+import {
+  currencies,
+  formatMoney,
+  slotsToDineros,
+  sumDineros,
+} from "~/monetary/math";
+import { createConverter } from "~/monetary/rsc";
+import { currentUser } from "~/auth";
 
 export default function PeriodsDetailsPage(props: {
   params: { periodId: string };
 }) {
   return (
-    <DashboardShell title="Period" description={props.params.periodId}>
-      Coming Soon...
+    <DashboardShell
+      title="Period"
+      description="This page is under construction"
+    >
+      <Suspense>
+        <PeriodsInformation id={props.params.periodId} />
+      </Suspense>
     </DashboardShell>
+  );
+}
+
+async function PeriodsInformation(
+  props: Readonly<{
+    id: string;
+  }>,
+) {
+  /**
+   * FIXME: Fetch by id
+   */
+  const periods = await trpc.getOpenPeriods();
+  const period = periods.find((p) => p.id === props.id);
+  if (!period) notFound();
+
+  const nSlots = period.timeslots.length;
+  const nHours = period.timeslots.reduce(
+    (acc, slot) => +slot.duration + acc,
+    0,
+  );
+
+  const converter = await createConverter();
+  const user = await currentUser();
+  const revenue = sumDineros({
+    dineros: slotsToDineros(period.timeslots),
+    currency: user!.defaultCurrency,
+    converter: converter.convert,
+  });
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex items-center gap-2">
+        <h2 className="font-bold text-lg">{period.client.name}</h2>
+        <p className="text-muted-foreground text-sm">
+          {formatOrdinal(period.startDate)}
+          {" to "}
+          {formatOrdinal(period.endDate)}
+        </p>
+        {isPast(period.endDate) && (
+          <Badge variant="destructive" className="ml-auto">
+            Expired
+          </Badge>
+        )}
+      </div>
+      <p className="text-sm">
+        {nSlots} reported times ({nHours}h) for a total of{" "}
+        <b>{toDecimal(revenue, formatMoney)}</b>
+      </p>
+      <ul>
+        {period.timeslots.map((slot) => {
+          const chargeRate = dinero({
+            amount: slot.chargeRate,
+            currency: currencies[slot.currency],
+          });
+
+          return (
+            <div key={slot.id}>
+              <p className="text-muted-foreground text-sm">
+                {formatOrdinal(slot.date)}
+                {" - "}
+                {slot.duration}
+                {"h @ "}
+                {toDecimal(chargeRate, (money) => formatMoney(money))}
+              </p>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
